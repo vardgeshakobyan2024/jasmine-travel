9af145aba1bb93bfc03e299246c8775d949a852e
vardgeshakobyan2024 <vardgeshakobyan5@gmail.com>
2024-03-16 13:09:57 +0300

    add components main and table

--- src/components/Site/Site.jsx
+++ src/components/Site/Site.jsx
@@ -1,10 +1,12 @@
 import HeaderSite from '../HeaderSite/HeaderSite';
+import MainSite from '../MainSite/MainSite';
 import './site.css';
 
 const Site = () => {
     return (
         <div className="site">
             <HeaderSite />
+            <MainSite />
         </div>
     )
 }
