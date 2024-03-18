1c7d1775430d6a8cd3dae023f568603b87e0f858
vardgeshakobyan2024 <vardgeshakobyan5@gmail.com>
2024-03-18 18:42:17 +0300

    add textBlock and hover

--- src/components/CardSliderItem/CardSliderItem.jsx
+++ src/components/CardSliderItem/CardSliderItem.jsx
@@ -5,6 +5,10 @@ const CardSliderItem = ({data, name}) => {
         <div className="cardSliderItem">
             <img src={data.image} alt={name}/>
             <h4>{name}</h4>
+
+            <div className="textBlock">
+                <p>{data.description}</p>
+            </div>
         </div>
     )
 }
