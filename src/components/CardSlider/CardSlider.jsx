e3b4ac5904dd3b55cae88df6ef5c044992b38103
vardgeshakobyan2024 <vardgeshakobyan5@gmail.com>
2024-03-19 17:37:42 +0300

    add teacher Milena id 4

--- src/components/CardSlider/CardSlider.jsx
+++ src/components/CardSlider/CardSlider.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import './cardSlider.css';
 import CardSliderItem from '../CardSliderItem/CardSliderItem';
 import teacherArtavazd from '../../images/artavazdTeacher.jpeg';
-import teacher from '../../images/teacher.jpeg';
+import teacherMelana from '../../images/melanaTeacher.jpeg';
+import teacherMariam from '../../images/mariamTeacher.jpeg';
+import teacherMilena from '../../images/milenaTeacher.jpg';
 
 const CardSlider = () => {
     const data = [
@@ -14,9 +16,21 @@ const CardSlider = () => {
         },
         {
             id: 2,
-            fullName: "Anonimus Anonimus",
+            fullName: "Մելանա Ավոյան",
             description: "Եթե ցանկանում եք լիարժեք սովորել AMADEUS ծրագիրը և տուրիզմի բնագավառում դառնալ գիտակ մարդ՝ միանշանակ պետք է դիմել JASMIN TRAVEL Հասմիկի մոտ դասընթացները անցնում են շատ հագեցած և նա իր ողջ գիտելիքները առանց պահումների փոխանցում է իր ուսանողներին",
-            image: teacher
+            image: teacherMelana
+        },
+        {
+            id: 3,
+            fullName: "Մարիամ Բալյան",
+            description: "2022թ. ինձ մոտ ցանկություն առաջացավ ուսումնասիրել զբոսաշրջային գործը, որը շատ բազմաշերտ և հետաքրքիր ոլորտ է: Ոլորտում կան շատ պրոֆեսիոնալներ, որոնք պատրաստ են նոր սկսնակներին ծանոթացնել աշխատանքային նրբություններին, բայց իմ ընտրությունը կանգ առավ <<Jasmine Travel>> տուրիստական ընկերության վրա Մինչ օրս շնորհակալ եմ ստացածս գիտելիքների համար, ու մեծագույն ուրախությամբ և պատասխանատվությամբ խորհուրդ կտամ ձեզ վստահել <<Jasmine Travel>>-ին:",
+            image: teacherMariam
+        },
+        {
+            id: 4,
+            fullName: "Միլենա Համբարձումյան",
+            description: "Ամենալավ աշխատանքներից մեկը , դա մարդկանց ճանապարհորդության ուղարկելն է: Անչափ հաճելի պրոցեսս է մանրամասներն երես առ երես քննարկելը, թղթաբանական հարցերով գրագետ զբաղվելը,հաշվի առնելով բոլոր նրբություններն տուր օպերատրի աշխատանքը համարվում է անչափ պատասխանատու : Այդ բոլոր հմտությունները կարելի է ձեռք բերել Jasmine Traveli ի օգնությամբ, այստեղ Ձեզ կօգնեն հեշտությամբ սովորել մասնագիտությունն ու ինչու՞ ոչ նաև ունենալ սեփական տուրիստականը",
+            image: teacherMilena
         }
     ]
     const [index, setIndex] = useState(0);
